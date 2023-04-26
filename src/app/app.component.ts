79d03b1e99b88ba0fc97a32c5faf0d5ecbac07ee
a13xl <106622336+a13xl@users.noreply.github.com>
2023-04-26 10:28:59 +0200

    added disabled clicked btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,20 @@ import { Component, ViewChildren, QueryList, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChildren('categoryBtn') categoryBtn: QueryList<ElementRef>;
+  @ViewChildren('categoryBtn', { read: ElementRef })
+  categoryBtn: QueryList<ElementRef>;
 
   name = 'Angular';
   categories = ['JS', 'Angular', 'Python'];
 
   disableCategoryBtn(id: number) {
-    console.log(id);
-    // this.categoryBtn.forEach((categoryBtn: ElementRef) => console.log(categoryBtn.nativeElement));
-    //console.log(this.categoryBtn[id].nativeElement);
+    this.enableBtns();
+    this.categoryBtn._results[id].nativeElement.disabled = true;
+  }
+
+  enableBtns() {
+    this.categoryBtn.forEach((categoryBtn: ElementRef) => {
+      categoryBtn.nativeElement.disabled = false;
+    });
   }
 }
